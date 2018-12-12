61df914c2ca96d7d669330343b39afa710904312
bzi1030765617 <bzi1030765617@gmail.com>
2018-12-12 15:22:21 +0000

    Builded the basic testing and added the first test to check if the clsCustomer is exists as example.

--- WindowsFormsApp1/BackEnd/Login.cs
+++ WindowsFormsApp1/BackEnd/Login.cs
@@ -21,27 +21,37 @@ namespace BackEnd
         {
             if (tbxAccount.Text == "" || tbxPassword.Text == "")
             {
+                //show error message if accont or password are blank
                 lblError.Text = "Please input your account and password.";
             }
             else
             {
+                //new an index object
                 Index index = new Index();
+                //hide this form
                 this.Hide();
+                //show index form
                 index.ShowDialog();
             }
         }
 
         private void linklblForgetPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            //new a forget password object
             ForgetPassword forgetPassword = new ForgetPassword();
+            //hide this form
             this.Hide();
+            //show forget password form
             forgetPassword.ShowDialog();
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
+            //new a register object
             Register register = new Register();
+            // hide this form
             this.Hide();
+            //show register form
             register.ShowDialog();
         }
     }
---
+++ WindowsFormsApp1/Test Framework/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void testInstanceOk()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
