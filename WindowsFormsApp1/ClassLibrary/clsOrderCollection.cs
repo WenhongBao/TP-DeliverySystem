cd83e6072e438df610f8b11a293993ca700ec4a1
bzi1030765617 <bzi1030765617@gmail.com>
2019-01-18 11:47:57 +0000

    Started to work on Order Collection. Builded some store function into database.

--- WindowsFormsApp1/ClassLibrary/clsOrderCollection.cs
+++ WindowsFormsApp1/ClassLibrary/clsOrderCollection.cs
@@ -1,6 +1,99 @@
-﻿namespace ClassLibrary1
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
 {
     public class clsOrderCollection
     {
+        //private data member for the list
+        private List<clsOrder> mOrderList = new List<clsOrder>();
+        //private data member thisTutor
+        clsOrder mThisOrder = new clsOrder();
+
+        public int Count { get; set; }
+        public List<clsOrder> OrderList
+        {
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+            }
+        }
+
+        //public property for ThisOrder
+        public clsOrder ThisOrder
+        {
+            get
+            {
+                //return the private data
+                return mThisOrder;
+            }
+            set
+            {
+                //set the private data
+                mThisOrder = value;
+            }
+        }
+
+        public clsOrderCollection()
+        {
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblTutor_SelectAll");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        public void FilterByDateCreated(string DateCreated)
+        {
+            //filters the records based on a full or partial first name
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the FirstName parameter to the database
+            DB.AddParameter("@DateCreated", DateCreated);
+            //execte the stored procedure
+            DB.Execute("sproc_tblOrder_FilterByDateCreated");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mOrderList = new List<clsOrder>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsOrder AnOrder = new clsOrder();
+                //read in the fields from the current record
+                AnOrder.OrderNo = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderNo"]);
+                AnOrder.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                AnOrder.CollectionPostcode = Convert.ToString(DB.DataTable.Rows[Index]["CollectionPostcode"]);
+                AnOrder.DestinationCountry = Convert.ToString(DB.DataTable.Rows[Index]["DestinationCountry"]);
+                AnOrder.DestinationPostcode = Convert.ToString(DB.DataTable.Rows[Index]["DestinationPostcode"]);
+                AnOrder.DateCreated = Convert.ToString(DB.DataTable.Rows[Index]["DateCreated"]);
+                AnOrder.ParcelSize = Convert.ToString(DB.DataTable.Rows[Index]["ParcelSize"]);
+                AnOrder.Status = Convert.ToString(DB.DataTable.Rows[Index]["Status"]);
+                AnOrder.CustomerNo = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerNo"]);
+                //add the record to the private data mamber
+                mOrderList.Add(AnOrder);
+                //point at the next record
+                Index++;
+            }
+        }
+
+        }
     }
-}
--- WindowsFormsApp1/Test Framework/tstOrderCollection.cs
+++ WindowsFormsApp1/Test Framework/tstOrderCollection.cs
@@ -19,5 +19,118 @@ namespace Test_Framework
             //test to see that it exists
             Assert.IsNotNull(AllOrder);
         }
+
+        //TEST PROPERTY OK
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instance of the class
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 1;
+            //assign the data to the property
+            AllOrder.Count = SomeCount;
+            //test to see that it exists
+            Assert.AreEqual(AllOrder.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void OrderListOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsOrder> TestList = new List<clsOrder>();
+            //add an item to the list
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //set its properties
+            TestItem.OrderNo = 1;
+            TestItem.Active = true;
+            TestItem.CollectionPostcode = "LE2 7EE";
+            TestItem.DestinationCountry = "United Kingdom";
+            TestItem.DestinationPostcode = "LE2 7FZ";
+            TestItem.DateCreated = "2018/02/01";
+            TestItem.ParcelSize = "M";
+            TestItem.Status="Dispatching";
+            TestItem.CustomerNo = 1;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllOrders.OrderList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisOrderPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            clsOrder TestItem = new clsOrder();
+            //set the properties of the test object
+            TestItem.OrderNo = 1;
+            TestItem.Active = true;
+            TestItem.CollectionPostcode = "LE2 7EE";
+            TestItem.DestinationCountry = "United Kingdom";
+            TestItem.DestinationPostcode = "LE2 7FZ";
+            TestItem.DateCreated = "2018/02/01";
+            TestItem.ParcelSize = "M";
+            TestItem.Status = "Dispatching";
+            TestItem.CustomerNo = 1;
+            //assign the data to the property
+            AllOrders.ThisOrder = TestItem;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.ThisOrder, TestItem);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsOrder> TestList = new List<clsOrder>();
+            //add an item to the list
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //set the properties of the test object
+            TestItem.OrderNo = 1;
+            TestItem.Active = true;
+            TestItem.CollectionPostcode = "LE2 7EE";
+            TestItem.DestinationCountry = "United Kingdom";
+            TestItem.DestinationPostcode = "LE2 7FZ";
+            TestItem.DateCreated = "2018/02/01";
+            TestItem.ParcelSize = "M";
+            TestItem.Status = "Dispatching";
+            TestItem.CustomerNo = 1;
+            //assign the data to the property
+            AllOrders.OrderList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.Count, TestList.Count);
+        }
+
+        //[TestMethod]
+        //public void AllTutorsOK()
+        //{
+        //    //create an instance of the class we want to create
+        //    clsOrderCollection AllOrders = new clsOrderCollection();
+        //    //create some test data to assign to the property
+        //    //in this case the data needs to be a list of objects
+        //    List<clsOrder> TestList = new List<clsOrder>();
+        //    //add an item to the list
+        //    //create the item of test data
+        //    clsOrder TestItem = new clsOrder();
+        //    TestItem.OrderNo = 1;
+        //    TestItem. = "Parker";
+        //    TestList.Add(TestItem);
+        //    Tutors.AllTutors = TestList;
+        //    Assert.AreEqual(Tutors.AllTutors, TestList);
+        //}
+
     }
 }
